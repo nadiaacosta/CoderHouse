61eb67d218d44e635f1e7ae9bfe4fad2758cc77f
Nadia Acosta <nadia.8acosta@gmail.com>
2022-12-07 21:58:28 -0300

    agregando sass

--- .vscode-R/init.R
+++
@@ -1,1 +0,0 @@
-local(source("c:\\Users\\Nadio\\.vscode\\extensions\\reditorsupport.r-2.5.2\\R\\session\\init.R", chdir = TRUE, local = TRUE))
--- js/script.js
+++
@@ -1,8 +0,0 @@
-const menu_responsive = document.querySelector(".menu_responsive");
-const navMenu = document.querySelector(".nav_menu");
-
-menu_responsive.addEventListener("click", () => {
-    menu_responsive.classList.toggle("active");
-    navMenu.classList.toggle("active");
-
-})
